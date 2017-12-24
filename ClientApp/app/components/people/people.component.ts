b25f337bd8a4465fd5549d2665139bd50021af7d
David Culham <culhamda@gmail.com>
2017-12-23 19:04:42 -0500

    Added Workers Grid

--- ClientApp/app/components/people/people.component.ts
+++ ClientApp/app/components/people/people.component.ts
@@ -1,9 +1,34 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITdDataTableColumn } from '@covalent/core';
+
+import { AssetService } from '../../services/asset.service';
+
+import { Worker } from '../calendar/common/models';
 
 @Component({
     selector: 'people',
     templateUrl: './people.component.html'
 })
-export class PeopleComponent {
-}
+export class PeopleComponent implements OnInit {
+    
+    constructor(
+        private assetService: AssetService
+    ) {
+    }
+
+    configColumns: ITdDataTableColumn[] = [
+      { name: 'name',  label: 'First Name' },
+      { name: 'lastName', label: 'Last Name' },
+      { name: 'phone', label: 'Phone Number' }
+    ];
+  
+    workers: Worker[];
+   
+    ngOnInit(){
+        //Get page
+        this.assetService.getWorkers().subscribe(result => {
+            this.workers = result;
+        })
+    }
+  }
 
