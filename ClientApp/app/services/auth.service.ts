c3f86261a7499c67999c84abf765f5e62ecc7cb2
David Culham <culhamda@gmail.com>
2018-01-14 14:09:45 -0500

    auth service work

--- ClientApp/app/services/auth.service.ts
+++ ClientApp/app/services/auth.service.ts
@@ -6,8 +6,15 @@ export class AuthService {
      
     constructor(private http: Http) {
     }
-      
-    login(email:string, password:string ) {
+    
+    private token: string = "";
+    private tokenExpiration: Date;
+
+    public get loginRequired(): boolean {
+        return this.token.length == 0 || this.tokenExpiration > new Date();
+    }
+
+    login(username:string, password:string ) {
         // return this.http.post<User>('/api/login', {email, password})
         //     // this is just the HTTP call, 
         //     // we still need to handle the reception of the token
