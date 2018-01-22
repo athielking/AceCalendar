beef01f9a63370407a295c4109d18c483fe632e9
Andrew Thielking <athielking@gmail.com>
2018-01-22 05:48:03 -0500

    fixed some issues from the merge

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { 
-  TdLayoutComponent, 
-  TdLayoutNavComponent, 
-  TdLayoutManageListComponent, 
-  TdLayoutManageListToggleDirective, 
-  TdNavigationDrawerComponent, 
-  TdLayoutFooterComponent 
+import {
+  TdLayoutComponent,
+  TdLayoutNavComponent,
+  TdLayoutManageListComponent,
+  TdLayoutManageListToggleDirective,
+  TdNavigationDrawerComponent,
+  TdLayoutFooterComponent
 } from '@covalent/core';
-import { 
+import {
   MatList,
   MatListItem,
   MatIcon,
@@ -25,16 +25,21 @@ import { AuthService } from "../../services/auth.service";
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  
+
   public title = 'app';
   public authorized: boolean;
   public loggedInUser: string;
+  public menuOpen: boolean = true;
 
-  constructor( private authService: AuthService ) {
-    this.authorized = !this.authService.loginRequired(); 
+  constructor(private authService: AuthService) {
+    this.authorized = !this.authService.loginRequired();
     this.loggedInUser = this.authService.getLoggedInUser();
   }
 
+  public toggleMenu(){
+    this.menuOpen = !this.menuOpen;
+  }
+  
   public logout(): void {
     this.authService.logout(
       () => {
