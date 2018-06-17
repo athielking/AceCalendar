693d0bdaeaf3596ef65163f945632743d659a75c
Andy Thielking <athielking@gmail.com>
2018-06-16 21:55:52 -0400

    working for updating other clients for single day type activities

--- ClientApp/app/services/signalr.service.ts
+++ ClientApp/app/services/signalr.service.ts
@@ -3,7 +3,12 @@ import { HubConnection } from '@aspnet/signalr';
 import * as signalR from '@aspnet/signalr';
 import { Observable, BehaviorSubject } from 'rxjs/Rx';
 
-import {environment} from '../../environments/environment';
+import { environment } from '../../environments/environment';
+import { CalendarService } from './calendar.service';
+import { StorageService } from './storage.service';
+import { StorageKeys } from '../components/calendar/common/calendar-tools';
+import { DayView } from '../components/calendar/common/models';
+import { ORGANIZATION_ID } from './auth.service';
 
 @Injectable()
 export class SignalrService {
@@ -12,25 +17,35 @@ export class SignalrService {
     private _hubUrl: string;
 
     private _connected: BehaviorSubject<boolean> = new BehaviorSubject(false);
-    private _dataUpdated: BehaviorSubject<Date> = new BehaviorSubject(null);
+    private _dataUpdated: BehaviorSubject<DayView[]> = new BehaviorSubject(null);
 
     public connected: Observable<boolean> = this._connected.asObservable();
-    public dataUpdated: Observable<Date> = this._dataUpdated.asObservable();
+    public dataUpdated: Observable<DayView[]> = this._dataUpdated.asObservable();
 
-    constructor(){
+    constructor(private calendarService: CalendarService,
+                private storageService: StorageService){
+
         this._hubUrl = environment.webServiceUrl + '/ws';
+        
     }
 
     public connect() : Observable<boolean> {
+
         if(!this._hubConnection)
             this._hubConnection = new signalR.HubConnectionBuilder()
                 .withUrl(this._hubUrl)
-                .configureLogging(signalR.LogLevel.Information)
+                .configureLogging(signalR.LogLevel.Error)
                 .build();
         
         var obs = Observable.from( this._hubConnection.start() );
         var sub = obs.subscribe( () => {
             this._connected.next(true);
+
+            this.registerHandlers();
+
+            if(this.storageService.hasItem(ORGANIZATION_ID))
+                this.addToGroup(this.storageService.getItem(ORGANIZATION_ID));
+
         }, error => {
             this._connected.next(false);
         }, () =>{sub.unsubscribe()});
@@ -47,17 +62,19 @@ export class SignalrService {
 
     private registerHandlers(){
         this._hubConnection.on("AddedToGroup", (id: string) => this.addedToGroup(id));
-        this._hubConnection.on("DataUpdated", (date: string) => this.onDataUpdated(date));
+        this._hubConnection.on("DataUpdated", (data: any) => this.onDataUpdated(data));
     }
 
     private addedToGroup(organizationId: string){
-        console.log("Added to server group " + organizationId);
+        console.log("SignalR -- Added to server group " + organizationId);
     }
 
-    private onDataUpdated(date: string) {
-        var ymd = date.substr(0, date.indexOf("T")).split("-");
-        let d: Date = new Date(+ymd[0], (+ymd[1])-1, +ymd[2]);
+    private onDataUpdated(data: any) {
 
-        this._dataUpdated.next(d);
+        console.log("SignalR -- Data Recieved server")
+        let viewDate = new Date(this.storageService.getItem(StorageKeys.viewDate));
+        var dayViews = this.calendarService.mapDayViewResponse(viewDate, {data: data});
+
+        this._dataUpdated.next(dayViews);
     }
 }
